2fa7c31f2095ba2bf1c23142cd81e653b4249604
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2022-03-28 21:24:54 +0300

    #5.4 5th charper
    add SimpleDotCom
    add SimpleDotComGame
    page 144

---
+++ src/SimpleDotComGame.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SimpleDotComGame {
+
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int NumOfGuesses = 0; //количество пользователей
+
+        SimpleDotCom DotCom = new SimpleDotCom();
+
+        int rand = (int) (Math.random() * 5); // генераци€ случайного числа
+        int [] locations = {rand, rand + 1, rand +2 };
+
+        DotCom.setLocationCells(locations); // передача местоположени€
+
+        boolean isAlive = true;
+
+        while (isAlive){
+            System.out.print("¬ведите число");
+            String guess = scanner.nextLine();
+
+            String result = DotCom.checkYourSelf(guess);
+            NumOfGuesses++;
+
+            if ( result.equals("ѕотопил")){
+                isAlive=false;
+                System.out.println("¬ам потребовалось " + NumOfGuesses + " попыток");
+            }
+        }
+
+    }
+}
