e327a60e909bcaaaf9903f338ccde516e0391725
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2022-08-26 12:16:50 +0300

    #11.1 page 173

--- src/MusicChapter11/MusicTest1.java
+++ src/MusicChapter11/MusicTest1.java
@@ -1,22 +1,43 @@
 package MusicChapter11;
 
-import javax.sound.midi.MidiSystem;
-
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Sequencer;
+import javax.sound.midi.*;
 
 public class MusicTest1 {
     public static void play()  {
         try {
             Sequencer sequencer = MidiSystem.getSequencer();// ОБъект синтезирует "композицию" из информации в формате MIDI
             System.out.println("Синтезатор был создан");
+            sequencer.open();
+
+            Sequence seq = new Sequence(Sequence.PPQ,4); // конструктор синтезатора
+
+            Track track = seq.createTrack();//Запрос трека у последовательности
+            //-------------------------------------------------
+            //-----------помещение в трек MIDI событий-------------
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144,1,44,100);
+            MidiEvent noteOn = new MidiEvent(a,1);
+            track.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            a.setMessage(128,1,44,100);
+            MidiEvent noteOff = new MidiEvent(b,16);
+            track.add(noteOff);
+            //-----------------------------------------------------------------
+            sequencer.setSequence(seq); // передача последовательности синтезатору
+
+            sequencer.start();
+
         }catch(MidiUnavailableException e){
             System.out.println(e.getMessage());
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
         }
 
     }
 
     public static void main (String [] args) {
+
         MusicTest1.play();
     }
 
